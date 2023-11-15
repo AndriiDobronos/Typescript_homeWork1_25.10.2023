ab3355e790120a5849f627468e4439e5dadd0a86
Andrii Dobronos <andriidobronos@gmail.com>
2023-11-15 19:34:21 +0200

    Homework5

--- src/index.ts
+++ src/index.ts
@@ -1,118 +1,58 @@
-//Створіть класи Circle, Rectangle, Square і Triangle.
-// У кожного з них є загальнодоступний метод calculateArea.
-// У кожної фігури є загальнодоступні властивості - колір
-// і назва, які не можна змінювати після створення.
-// У Square і Rectangle зі свого боку є ще додатковий метод
-// print, який виводить рядок із формулою розрахунку площі.
+//Визначте інтерфейс, який використовує сигнатуру індексу з типами об'єднання.
+// Наприклад, тип значення для кожного ключа може бути число | рядок.
 
-interface IShape {
-    name: string ;
-    color: string;
-    calculateArea():number ;
+// Створіть інтерфейс, у якому типи значень у сигнатурі індексу є функціями.
+// Ключами можуть бути рядки, а значеннями — функції, які приймають будь-які аргументи.
+
+// Опишіть інтерфейс, який використовує сигнатуру індексу для опису об'єкта,
+// подібного до масиву. Ключі повинні бути числами, а значення - певного типу.
+
+// Створіть інтерфейс з певними властивостями та індексною сигнатурою.
+// Наприклад, ви можете мати властивості типу name: string та індексну сигнатуру
+// для додаткових динамічних властивостей.
+
+// Створіть два інтерфейси, один з індексною сигнатурою, а інший розширює перший,
+// додаючи специфічні властивості.
+
+// Напишіть функцію, яка отримує об'єкт з індексною сигнатурою і перевіряє, чи
+// відповідають значення певних ключів певним критеріям (наприклад, чи всі значення є числами).
+
+interface ITestInterface {
+   [keyWord: string]: number | string;
 }
 
-abstract class Shape {
-    protected constructor(
-        public readonly name: string = "Shape",
-        public readonly color: string = "transparent",
-    ) {}
-
-    public calculateArea(): number {
-        return 1
-    };
+interface IFuncInterface {
+    [key : string]: (...args: any[]) => any;
 }
 
-interface ICircle extends IShape {
-    circleArea:number;
+type PossibleType = number | string | boolean ;// Вказуемо тип значень, які очікуємо
+interface IArrayLikeObject {
+    [index: number]: PossibleType ;
 }
 
-class Circle extends Shape implements ICircle {
-    constructor(public readonly name: string,
-                public readonly color: string,
-                private radius:number,
-    ) {
-        super( name, color)
+interface ICustomObject {
+    name: string;
+    [key: string]: any;
+}
+
+interface IBaseInterface {
+    [key: string]: any;
+}
+
+interface IUniqueInterface extends IBaseInterface {
+    description: string;
+    price: number;
+}
+
+interface IIndexSignatureObject {
+    [key: string]: any;
+}
+
+function areValuesNumbers(obj: IIndexSignatureObject, keys: string[]): boolean {
+    for (const key of keys) {
+        if (typeof obj[key] !== 'number') {
+            return false;
+        }
     }
-    calculateArea(r:number = this.radius):number {
-        return Math.PI*Math.pow(r,2)
-    }
-    circleArea:number = this.calculateArea()
+    return true;
 }
-
-const circle = new Circle("bigCircle","yellow",6)
-console.log(circle,circle.circleArea)
-
-interface IPrintable {
-    print():string;
-}
-
-interface IRectangle extends IShape,IPrintable {
-    rectangleArea:number ;
-}
-
-class Rectangle extends Shape implements IRectangle {
-    constructor(public readonly name: string,
-                public readonly color: string,
-                private sideA:number,
-                private sideB: number
-    ) {
-        super( name, color)
-    }
-    calculateArea(a:number=this.sideA ,b:number=this.sideB ):number {
-        return (a*b) ;
-    }
-    print():string {
-        return "RectangleArea = sideA * sideB"
-    };
-    rectangleArea:number =  this.calculateArea()
-}
-
-const rectangle = new Rectangle("bigRectangle","red",6,7)
-console.log(rectangle,rectangle.rectangleArea)
-
-interface ISquare extends IShape,IPrintable {
-    squareArea:number;
-}
-
-class Square extends Shape implements ISquare {
-    constructor(public readonly name: string,
-                public readonly color: string,
-                private sideLength:number
-    ) {
-        super( name, color)
-    }
-
-    calculateArea(c:number=this.sideLength):number {
-        return (Math.pow(c,2)) ;
-    }
-
-    print():string {
-        return "SquareArea = Math.pow(sideLength,2) "
-    };
-    squareArea:number = this.calculateArea()
-}
-
-const square = new Square("bigSquare","green",7)
-console.log(square ,square.calculateArea())
-
-interface ITriangle extends IShape {
-    triangleArea:number ;
-}
-
-class Triangle extends Shape implements ITriangle {
-    constructor(public readonly name: string,
-                public readonly color: string,
-                private sideA:number,
-                private sideB:number,
-                private sizeOfAngle:number,
-    ) {
-        super( name, color)
-    }
-    calculateArea(a:number = this.sideA,b:number = this.sideB,c:number = this.sizeOfAngle):number {
-        return ((a*b)/2)*(Math.sin(c*0.0174533))
-    }
-    triangleArea:number = this.calculateArea()
-}
-
-const triangle = new Triangle("bigTriangle","blue",6,7,45)
-console.log(triangle,triangle.triangleArea)
